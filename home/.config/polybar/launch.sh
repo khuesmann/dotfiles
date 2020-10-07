f024e4eac6a2497528d3684bc4fb9d6a5f42ef23
karim <k>
2020-10-07 21:58:09 +0200

    polybar

---
+++ home/.config/polybar/launch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+BAR_NAME=base
+BAR_CONFIG=$HOME/.config/polybar/config
+
+PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
+OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+
+# Launch on primary monitor
+MONITOR=$PRIMARY polybar --reload -c $BAR_CONFIG $BAR_NAME &
+sleep 1
+
+# Launch on all other monitors
+for m in $OTHERS; do
+ MONITOR=$m polybar --reload -c $BAR_CONFIG $BAR_NAME &
+done
